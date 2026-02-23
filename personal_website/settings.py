6315115daf4bcb462f4f3afce01420099b36ccb7
Adel Ghaedi <adelghaedii12@gmail.com>
2026-02-23 22:50:09 +0330

    [UP] requirements for postgresql

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,15 +86,19 @@ WSGI_APPLICATION = 'personal_website.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'personal-website-db',
+#         'USER': 'root',
+#         'PASSWORD': 'QpRAMAzDtmHuR163f1UYBTIc',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'personal-website-db',
-        'USER': 'root',
-        'PASSWORD': 'QpRAMAzDtmHuR163f1UYBTIc',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
